a94d163ef1eab5858536f64e3f96a08028298225
hiro0911 <evergreen.0911@gmail.com>
2020-05-05 09:34:10 +0900

    [Finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8ee60e2f83cbebb0eabf90928fc7a261f3ff4f3b0cd1ff0944ecf2d0f331f4949a3f18b5b4a7e1c30c0153e7196e4fb1ea9885d6ae78d6f971fc304a985416a6'
